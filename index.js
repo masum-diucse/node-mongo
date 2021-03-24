566a9cc79791ba1635ff0cf654c0948f284fea2d
Masum-Web-Developer <masum.diucse@gmail.com>
2021-03-24 19:02:28 +0600

    update

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
-
-app.get('/',(req,res)=>res.send("Thank you for calling me!!"));
+const fruits={
+    product:"Banana"
+}
+const users=["Asad","based","Kashed","Rashed","Nashed"];
+app.get('/',(req,res)=>res.send(fruits));
+app.get('/fruits/banana',(req,res)=>{
+    res.send({fruit:"pineapple",quantity:100,price:10000});
+})
+app.get('/users/:id',(req,res)=>{
+    const userId=req.params.id;
+    const name=users[userId];
+   res.send({userId,name});
+   console.log(req.params);
+   console.log(req.query.search);
+})
 
 app.listen(3000,()=>console.log("listening from 3000 port"));
