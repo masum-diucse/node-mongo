b89830d0e0c5bde057bef4d0c7e5258424c31899
Masum-Web-Developer <masum.diucse@gmail.com>
2021-03-24 18:01:30 +0600

    my first node project

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+app.get('/',(req,res)=>res.send("Thank you for calling me!!"));
+
+app.listen(3000,()=>console.log("listening from 3000 port"));
